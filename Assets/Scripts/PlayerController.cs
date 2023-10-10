1906668d4066f75152f9030ffcf0143bde186c69
Dean <deanmartel2001@outlook.com>
2023-10-10 13:00:36 -0400

    Ch6-Added 'Walking' animation to SpaceMarine & Updated 'PlayerController'script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     public Rigidbody head;
     public LayerMask layerMask;
     private Vector3 currentLookTarget = Vector3.zero;
+    public Animator bodyAnimator;
 
     // Start is called before the first frame update
     void Start()
@@ -32,11 +33,12 @@ public class PlayerController : MonoBehaviour
 
         if (moveDirection == Vector3.zero)
         {
-            //Todo
+            bodyAnimator.SetBool("IsMoving", false);
         }
         else
         {
             head.AddForce(transform.right * 150, ForceMode.Acceleration);
+            bodyAnimator.SetBool("IsMoving", true);
         }
 
         RaycastHit hit;
