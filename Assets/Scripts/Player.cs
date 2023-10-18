2a3021dc15dd18f191d63a8a7bae42058cffb1be
Dean <deanmartel2001@outlook.com>
2023-10-18 19:43:19 -0400

    Ch7-Added Background Music To Main Camera

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player
+{
+    private int experience;
+
+    public int Experience
+    {
+        get
+        {
+            return experience;
+        }
+        set
+        {
+            experience = value;
+        }
+    }
+}
